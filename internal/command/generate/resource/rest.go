9c7d02742ed5d87e23676dfaeb854b9a26977213
charmingruby <mrrubycanal3105@gmail.com>
2024-11-24 00:37:54 -0300

    feat: makes make rest public method

--- internal/command/generate/resource/rest.go
+++ internal/command/generate/resource/rest.go
@@ -22,36 +22,40 @@ func RunRest(projectData, destinationDirectory string) *cobra.Command {
 				panic(err)
 			}
 
-			moduleDir := fmt.Sprintf("%s/%s", destinationDirectory, module)
-
-			if err := fs.GenerateNestedDirectories(
-				moduleDir,
-				[]string{"transport", "rest", "endpoint"},
-			); err != nil {
-				panic(err)
-			}
-
-			if err := fs.GenerateFile(makeRestRegistryBrickComponent(
-				fmt.Sprintf("%s/%s", moduleDir, "transport/rest/endpoint"),
-				fmt.Sprintf("%s/%s", projectData, destinationDirectory),
-				module,
-			)); err != nil {
-				panic(err)
-			}
-
-			if err := fs.GenerateFile(brick.MakeHandlerComponent(
-				destinationDirectory,
-				module,
-				"ping",
-			)); err != nil {
-				panic(err)
-			}
+			MakeRest(projectData, destinationDirectory, module)
 		},
 	}
 
 	cmd.Flags().StringVarP(&module, "module", "m", "", "module name")
 
 	return cmd
+}
+
+func MakeRest(projectData, destinationDirectory, module string) {
+	moduleDir := fmt.Sprintf("%s/%s", destinationDirectory, module)
+
+	if err := fs.GenerateNestedDirectories(
+		moduleDir,
+		[]string{"transport", "rest", "endpoint"},
+	); err != nil {
+		panic(err)
+	}
+
+	if err := fs.GenerateFile(makeRestRegistryBrickComponent(
+		fmt.Sprintf("%s/%s", moduleDir, "transport/rest/endpoint"),
+		fmt.Sprintf("%s/%s", projectData, destinationDirectory),
+		module,
+	)); err != nil {
+		panic(err)
+	}
+
+	if err := fs.GenerateFile(brick.MakeHandlerComponent(
+		destinationDirectory,
+		module,
+		"ping",
+	)); err != nil {
+		panic(err)
+	}
 }
 
 type restRegistryBrickData struct {
